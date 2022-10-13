2a719fdbdd0c7c25efeb639852a7801c25f7a136
KEHyeon <kth050213@naver.com>
2022-10-13 13:21:20 +0900

    add:대회, 문제삭제

--- src/contest/contest.controller.ts
+++ src/contest/contest.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Delete, UseGuards } from '@nestjs/common';
 import { CreateContestDTO } from './dto/request/create-contest.dto';
 import { ContestService } from './contest.service';
 import { CreateProblemDTO } from './dto/request/create-problem.dto';
 import { GetProblemDTO } from './dto/request/get-problem.dto';
+import { DeleteProblemDTO } from './dto/request/delete-problem.dto';
+import { GetUser } from 'src/auth/getUser.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { CreateUserDTO } from 'src/user/dto/request/create-user.dto';
+import { User } from 'src/auth/auth.model';
 
 type idType = {
     id: number
@@ -18,7 +23,7 @@ type Contests = {
 export class ContestController {
     constructor(private readonly contestservice: ContestService) { }
 
-    @Post('create')
+    @Post('')
     createContest(
         @Body() dto: CreateContestDTO
     ) {
@@ -40,7 +45,7 @@ export class ContestController {
         return this.contestservice.GetEndedContests();
     }
 
-    @Get('problem/:id')
+    @Get('problem/:contestId')
     getProblemById(@Param() dto: GetProblemDTO) {
         return this.contestservice.GetProblemsById(dto);
     }
@@ -48,6 +53,26 @@ export class ContestController {
     @Post('problem')
     createProblemById(@Body() dto: CreateProblemDTO) {
         return this.contestservice.CreateContestProblemById(dto);
+    }
+    @Delete('problem')
+    deleteProblemById(@Body() dto: DeleteProblemDTO) {
+        return this.contestservice.DeleteContestProblemById(dto);
+    }
+    @Delete('/:contestid')
+    deleteContestById(@Param() params: {contestid: number}) {
+        return this.contestservice.DeleteContest(params.contestid);
+    }
+
+    @Post('/user')
+    @UseGuards(JwtAuthGuard) 
+    createUser(@Body() dto:CreateUserDTO) {
+        return this.contestservice.CreateUser(dto);
+    }
+    
+    @Delete('/user/:userId')
+    @UseGuards(JwtAuthGuard)
+    deleteUser(@Body() dto:DeleteUserDTO) {
+        return this.contestservice.DeleteUser(dto);
     }
 
 }
